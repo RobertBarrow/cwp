3b3f141448db076f78b65537dcd926408ea0e57e
Robert Barrow <RobertBarrow@users.noreply.github.com>
2018-04-04 16:04:49 +0100

    Improved enumeration using enum module and Enum class.

--- python/sb.py
+++ python/sb.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python
 import csv # for reading in source CWP.CSV source file
 import json # for storing data in json format
+from enum import Enum
 
 # Enumerations
-SOURCE_ID_COLUMN = 0
-SOURCE_NAME_COLUMN = 1
-SOURCE_PARENT_COLUMN = 2
-SOURCE_SPEND_COLUMN = 3
+class Column(Enum):
+    ID = 0
+    NAME = 1
+    PARENT = 2
+    SPEND = 3
 
-NAME = 0
-PARENT_ID = 1
-SPEND_ARRAY = 2
+class Position(Enum):
+    NAME = 0
+    PARENT_ID = 1
+    SPEND = 2
 
-MAX = 10 # should be 1 + the maximum number of downline levels to calculate
+# Global
+_MAX = 10 # should be 1 + the maximum number of downline levels to calculate
 
 # Function definitions
 def validParentID ( validParentID_in ):
@@ -24,10 +28,10 @@ def validParentID ( validParentID_in ):
 def addSpend ( addSpend_consultant_id, addSpend_level, addSpend_in):
     "Adds addSpend_in to addSpend_level for addSpend_consultant_id in myConsultants"
     if addSpend_in is not None:
-        if myConsultants[addSpend_consultant_id][SPEND_ARRAY][addSpend_level] == None:
-            myConsultants[addSpend_consultant_id][SPEND_ARRAY][addSpend_level] = addSpend_in
+        if myConsultants[addSpend_consultant_id][Position.SPEND][addSpend_level] == None:
+            myConsultants[addSpend_consultant_id][Position.SPEND][addSpend_level] = addSpend_in
         else:   
-            myConsultants[addSpend_consultant_id][SPEND_ARRAY][addSpend_level] += addSpend_in
+            myConsultants[addSpend_consultant_id][Position.SPEND][addSpend_level] += addSpend_in
     return
 
 # Read CSV file into sourceData
@@ -48,17 +52,17 @@ in_data.close()
 # Load sourceData into myConsultants
 myConsultants = {}
 for row in sourceData:
-    consultant_id = int(sourceData[row][SOURCE_ID_COLUMN])
-    name = sourceData[row][SOURCE_NAME_COLUMN]
-    parent_id = validParentID(sourceData[row][SOURCE_PARENT_COLUMN])
-    spend = [float(0) for x in range(MAX + 1)] # Initialise array
-    spend[0] = float(sourceData[row][SOURCE_SPEND_COLUMN])
+    consultant_id = int(sourceData[row][Column.ID])
+    name = sourceData[row][Column.NAME]
+    parent_id = validParentID(sourceData[row][Column.PARENT])
+    spend = [float(0) for x in range(_MAX + 1)] # Initialise array
+    spend[0] = float(sourceData[row][Column.SPEND])
     
     # Check for duplicate consultant ID
     if consultant_id in myConsultants:
         print("Warning: duplicate consultant ID", consultant_id)
-        if myConsultants[consultant_id][PARENT_ID] == None:
-            myConsultants[consultant_id][PARENT_ID] = parent_id
+        if myConsultants[consultant_id][Position.PARENT_ID] == None:
+            myConsultants[consultant_id][Position.PARENT_ID] = parent_id
         addSpend(consultant_id, 0, spend)
     else:
         myConsultants[consultant_id] = [name, parent_id, spend]
@@ -73,34 +77,34 @@ if len(myConsultants) != len(sourceData):
 sourceData = ''
           
 # Add consultant downline spend to parent record
-for x in range(MAX):
+for x in range(_MAX):
     y = x + 1
     for key,value in myConsultants.items() :
-        parent_id = value[PARENT_ID]
+        parent_id = value[Position.PARENT_ID]
         if parent_id is not None and parent_id in myConsultants:
-            downline_spend = value[SPEND_ARRAY][x]
+            downline_spend = value[Position.SPEND][x]
             if downline_spend is not None:
                 addSpend(parent_id, y, downline_spend)
 
 # Initialise totals
-total_spend = [float(0) for x in range(MAX)]
+total_spend = [float(0) for x in range(_MAX)]
 
 # Output results to OUT.CSV
 with open("OUT.CSV", 'w') as out:
     # Column headings
     out.write("Consultant,Name,Parent,Spend")
-    for x in range(1, MAX):
+    for x in range(1, _MAX):
         out.write(",Level_{}".format(x))
     out.write("\n")
     # Rows
     for key, value in myConsultants.items() :
         # Tidy up ParentID (replace <None> with <blank>) and output ID, Name and ParentID 
-        if value[PARENT_ID] == None: value[PARENT_ID] = ''
-        out.write("{},\"{}\",{}".format(key, value[NAME], value[PARENT_ID]))
+        if value[Position.PARENT_ID] == None: value[Position.PARENT_ID] = ''
+        out.write("{},\"{}\",{}".format(key, value[Position.NAME], value[Position.PARENT_ID]))
         # Update total_spend array and output values
-        for x in range(0, MAX):
-            total_spend[x] += value[SPEND_ARRAY][x]
-            out.write(",{:.2f}".format(round(value[SPEND_ARRAY][x], 2)))
+        for x in range(0, _MAX):
+            total_spend[x] += value[Position.SPEND][x]
+            out.write(",{:.2f}".format(round(value[Position.SPEND][x], 2)))
         # Newline
         out.write("\n")
 # Close OUT.CSV file        
@@ -113,5 +117,5 @@ out_data.close()
 
 # Output totals ( stdout )
 print ("Number of consultants   {}".format(len(myConsultants)))
-for x in range(0, MAX):
+for x in range(0, _MAX):
     print ("Total spend level {} GBP {:,.2f}".format(x, total_spend[x]))
